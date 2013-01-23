9a8aaa4f8625da014646a3774e50f49205d729c0
raghavendra-prithvi <shiva@searchexperiences.com>
2013-01-23 10:36:41 +0530

    git push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Tapski::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( structure.css,style.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
